b6cc27074b3d9365205be38bf539ffa684cffb81
Michael Rehman <michael@michaelrehman.com>
2023-02-04 15:09:49 -0500

    Working map

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
 import { Loader } from '@googlemaps/js-api-loader';
+import { RouterView } from 'vue-router';
 
 const loader = new Loader({
-	apiKey: 'AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg',
-	// version: 'weekly',
-	// libraries: ['places',],
+	apiKey: 'AIzaSyCC_j2mEQE9kFIL_mV095_lQwoOf13VA7g',
+	version: 'weekly',
+	libraries: ['places',],
 });
 
 loader.load().then((google) => {
 	// The location of Uluru
-	const uluru = { lat: 25.344, lng: 131.031, };
+	const uluru = { lat: -25.344, lng: 131.031, };
 	// The map, centered at Uluru
 	const map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 4,
@@ -28,6 +29,7 @@ loader.load().then((google) => {
 		<h1>shalom</h1>
 	</header>
 	<div id="map" style="width: 400px; height: 400px;"></div>
+	<RouterView />
 </template>
 
 <style scoped>
