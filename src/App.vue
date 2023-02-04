53fa481d331ee57158076b454d7daa8659c70ce6
Michael Rehman <michael@michaelrehman.com>
2023-02-04 15:18:03 -0500

    UI Strucutre

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script setup>
+import {
+	NConfigProvider, darkTheme,
+	NMessageProvider,
+	NLayout,
+	NLayoutHeader,
+	NLayoutContent,
+	NSpace
+} from 'naive-ui';
+
 import { Loader } from '@googlemaps/js-api-loader';
-import { RouterView } from 'vue-router';
 
 const loader = new Loader({
 	apiKey: 'AIzaSyCC_j2mEQE9kFIL_mV095_lQwoOf13VA7g',
@@ -8,28 +16,43 @@ const loader = new Loader({
 	libraries: ['places',],
 });
 
-loader.load().then((google) => {
-	// The location of Uluru
-	const uluru = { lat: -25.344, lng: 131.031, };
-	// The map, centered at Uluru
-	const map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 4,
-		center: uluru,
-	});
-	// The marker, positioned at Uluru
-	const marker = new google.maps.Marker({
-		position: uluru,
-		map: map,
-	});
-});
+// loader.load().then((google) => {
+// 	// The location of Uluru
+// 	const uluru = { lat: -25.344, lng: 131.031, };
+// 	// The map, centered at Uluru
+// 	const map = new google.maps.Map(document.getElementById('map'), {
+// 		zoom: 4,
+// 		center: uluru,
+// 	});
+// 	// The marker, positioned at Uluru
+// 	const marker = new google.maps.Marker({
+// 		position: uluru,
+// 		map: map,
+// 	});
+// });
 </script>
 
 <template>
-	<header>
-		<h1>shalom</h1>
-	</header>
-	<div id="map" style="width: 400px; height: 400px;"></div>
-	<RouterView />
+	<n-config-provider :theme="darkTheme">
+		<n-message-provider>
+
+			<n-layout>
+				<n-layout-header>
+					<n-space justify="space-around" align="center">
+						<header style="display: inline-block;">
+							<h1 style="display: inherit;">Map Map</h1>
+						</header>
+					</n-space>
+				</n-layout-header>
+
+				<n-layout-content>
+					<div id="map" style="width: 400px; height: 400px;"></div>
+					<router-view />
+				</n-layout-content>
+			</n-layout>
+
+		</n-message-provider>
+	</n-config-provider>
 </template>
 
 <style scoped>
